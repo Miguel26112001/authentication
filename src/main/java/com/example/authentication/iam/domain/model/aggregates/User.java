4cd05da05c394222ce476eb8dee31c052627bb6d
Miguel26112001 <u202220294@upc.edu.pe>
2025-12-22 10:41:24 -0500

    Add User aggregate for authentication module with role management support.

---
+++ src/main/java/com/example/authentication/iam/domain/model/aggregates/User.java
@@ -0,0 +1,56 @@
+package com.example.authentication.iam.domain.model.aggregates;
+
+import com.example.authentication.iam.domain.model.entities.Role;
+import com.example.authentication.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+public class User extends AuditableAbstractAggregateRoot<User> {
+  @NotBlank
+  @Size(max = 50)
+  @Column(unique = true)
+  private String username;
+
+  @NotBlank
+  @Size(max = 100)
+  private String hashedPassword;
+
+  @ManyToMany(
+      fetch = FetchType.EAGER,
+      cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
+  @JoinTable(
+      name = "user_roles",
+      joinColumns = @JoinColumn(name = "user_id"),
+      inverseJoinColumns = @JoinColumn(name = "role_id"))
+  private Set<Role> roles;
+
+  public User() {
+    this.roles = new HashSet<>();
+  }
+
+  public User(String username, String hashedPassword) {
+    this.username = username;
+    this.hashedPassword = hashedPassword;
+    this.roles = new HashSet<>();
+  }
+
+  public User(String username, String hashedPassword, List<Role> roles) {
+    this(username, hashedPassword);
+    addRoles(roles);
+  }
+
+  public void addRoles(List<Role> roles) {
+    var validatedRoleSet = Role.validateRoleSet(roles);
+    this.roles.addAll(validatedRoleSet);
+  }
+}
