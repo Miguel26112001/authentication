4339a996587e3e5943bfb7ce030c4e2820a11926
Miguel26112001 <u202220294@upc.edu.pe>
2025-12-22 10:29:17 -0500

    Add Role entity and Roles enum for role management in the authentication module.

---
+++ src/main/java/com/example/authentication/iam/domain/model/entities/Role.java
@@ -0,0 +1,47 @@
+package com.example.authentication.iam.domain.model.entities;
+
+import com.example.authentication.iam.domain.model.valueobjects.Roles;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@With
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(length = 20, unique = true)
+  private Roles roles;
+
+  public Role(Roles roles) {
+    this.roles = roles;
+  }
+
+  public String getStringRole() {
+    return this.roles.name();
+  }
+
+  public static Role getDefaultRole() {
+    return new Role(Roles.ROLE_USER);
+  }
+  public static Role toRoleFromName(String roleName) {
+    return new Role(Roles.valueOf(roleName));
+  }
+
+  public static List<Role> validateRoleSet(List<Role> roles) {
+    if (roles == null || roles.isEmpty()) {
+      return List.of(getDefaultRole());
+    }
+    return roles;
+  }
+}
