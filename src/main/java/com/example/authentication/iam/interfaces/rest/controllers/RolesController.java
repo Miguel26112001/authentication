3afbc81f16fd12f2969e33012f44cd411b122347
Miguel26112001 <u202220294@upc.edu.pe>
2025-12-23 00:23:18 -0500

    Refactor: merge `getAllRoles` and `getRoleByName` into single endpoint in `RolesController`

--- src/main/java/com/example/authentication/iam/interfaces/rest/controllers/RolesController.java
+++ src/main/java/com/example/authentication/iam/interfaces/rest/controllers/RolesController.java
@@ -3,7 +3,6 @@ package com.example.authentication.iam.interfaces.rest.controllers;
 import com.example.authentication.iam.domain.model.queries.GetAllRolesQuery;
 import com.example.authentication.iam.domain.model.queries.GetRoleByNameQuery;
 import com.example.authentication.iam.domain.services.RoleQueryService;
-import com.example.authentication.iam.interfaces.rest.resources.RoleResource;
 import com.example.authentication.iam.interfaces.rest.transform.RoleResourceFromEntityAssembler;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.MediaType;
@@ -12,8 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/v1/roles", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -26,27 +23,19 @@ public class RolesController {
   }
 
   @GetMapping
-  public ResponseEntity<List<RoleResource>> getAllRoles() {
-    var getAllRolesQuery = new GetAllRolesQuery();
-    var roles = roleQueryService.handle(getAllRolesQuery);
-    var roleResources = roles.stream()
-        .map(RoleResourceFromEntityAssembler::toResourceFromEntity)
-        .toList();
-    return ResponseEntity.ok(roleResources);
-  }
-
-
-  @GetMapping(params = "name")
-  public ResponseEntity<RoleResource> getRoleByName(@RequestParam("name") String name) {
-    var getRoleByNameQuery = new GetRoleByNameQuery(name);
-
-    var role = roleQueryService.handle(getRoleByNameQuery);
-
-    if (role.isEmpty()) {
-      return ResponseEntity.notFound().build();
+  public ResponseEntity<?> getRoles(@RequestParam(name = "name", required = false) String name) {
+    if (name == null || name.isBlank()) {
+      var getAllRolesQuery = new GetAllRolesQuery();
+      var roles = roleQueryService.handle(getAllRolesQuery);
+      var roleResources = roles.stream()
+          .map(RoleResourceFromEntityAssembler::toResourceFromEntity)
+          .toList();
+      return ResponseEntity.ok(roleResources);
     }
+    var getRoleByNameQuery = new GetRoleByNameQuery(name);
+    var role = roleQueryService.handle(getRoleByNameQuery);
+    if (role.isEmpty()) return ResponseEntity.notFound().build();
     var roleResource = RoleResourceFromEntityAssembler.toResourceFromEntity(role.get());
-
     return ResponseEntity.ok(roleResource);
   }
 }
